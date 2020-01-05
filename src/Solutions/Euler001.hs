424fc2a7169652b6c6ba482258bf3b4f16934b9a
a-wiacek <33523701+a-wiacek@users.noreply.github.com>
2020-01-06 00:54:37 +0100

    Rewrite solution 1

--- src/Solutions/Euler001.hs
+++ src/Solutions/Euler001.hs
@@ -1,8 +1,8 @@
 module Solutions.Euler001 where
-import Utils.Operators((<||>))
 
-zMod :: Int -> Int -> Bool
-zMod r x = x `mod` r == 0
+-- k + 2k + ... + k (bound `div` k) = k (1 + ... + bound `div` k)
+sumDivisibleUpTo :: Int -> Int -> Int
+sumDivisibleUpTo k bound = let l = bound `div` k in k * l * (l + 1) `div` 2
 
 euler001 :: IO String
-euler001 = return $ show $ sum $ filter (zMod 3 <||> zMod 5) [1..999]
+euler001 = return $ show $ sumDivisibleUpTo 3 999 + sumDivisibleUpTo 5 999 - sumDivisibleUpTo 15 999
